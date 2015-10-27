276e820fb42778fbfc5acf9f4b760cfbeac4605a
ESPECTRUS <edwincon85@hotmail.com>
2015-10-27 13:25:45 -0430

    INICIALIZANDO EL PROYECTO

---
+++ application/views/archivo_personal/personal_view.php
@@ -0,0 +1,115 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Archivo Central</title>
+		<meta name="Description" content=""/>
+		<meta name="Keywords" content=""/>
+		<meta name="Author" content=""/>
+		<meta name="Robots" content=""/>
+		<meta http-equiv="imagetoolbar" content="no"/>
+		<link rel="stylesheet" media="screen" href="<?= base_url('templates/css/style_general.css');?>"/>
+		<link rel="stylesheet" media="screen" href="<?= base_url('templates/css/style_menu.css');?>"/>
+		<!--<link rel="stylesheet" media="screen" href="<?= base_url('templates/css/ico_menu.css');?>"/>-->
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	</head>
+<body>
+<div id="daddy">
+	<!-- header -->
+	<div id="header">
+		<div id="logo">
+			<a href=""><img src="<?= base_url('templates/img/images/logo.png');?>" alt="" width="318px" height="85px" /></a>
+			<span id="logo-text"><a href=""></a></span>
+		</div>
+		<!-- menu -->
+		<div id="menu">
+			<ul>
+				<li><a href="" id="active">Principal</a></li>
+				<li><a href="">C.S.I.</a></li>
+				<li><a href="">C.P.I.</a></li>
+				<li><a href="">Solicitudes</a></li>
+				<li><a href="">Notificaciones</a></li>
+				<li><a href="">Devoluciones</a></li>
+			</ul>
+		</div>
+		<!-- ticker -->
+		<div id="ticker">
+			<b>Unidad Ejecutora de Titulación</b> del Ministerio de Obas Públicas, Servicios y Vivienda.
+		</div>
+		<div class="clear"></div>
+		<!-- headerimage -->
+		<div id="headerimage">
+			<!-- download -->
+			<div id="download">
+				<center>
+					<div id="user-icon">
+						<img src="<?= base_url('templates/img/images/user-men.png');?>">
+					</div>
+					<span id="download-text"><a href="">eajahuanca.uet<br/>Edwin Ajahuanca Callisaya</a></span>
+				</center>
+			</div>
+			<!-- icons -->
+			<div id="icons">
+				<a href="" ><img src="<?= base_url('templates/img/images/icon_home.gif');?>" title="Principal" width="13" height="13" id="home" /></a>
+				<a href=""><img src="<?= base_url('templates/img/images/icon_sitemap.gif');?>" title="Configuraciones" width="13" height="13" id="sitemap" /></a>
+				<a href=""><img src="<?= base_url('templates/img/images/icon_contact.gif');?>" title="Correo Electrónico" width="13" height="13" id="contact" /></a>			
+			</div>
+			<div id="slogan">UNIDAD EJECUTORA DE TITULACION</div>
+		</div>
+	</div>
+	
+	<div id="content">
+		<div id="cA">
+			<nav class="vnav2">
+	            <ul class="vnav-menu2">
+	                <li class="active2">
+	                    <a href="" class="vnav-item2">INICIO<span class="vnav-counter2">1</span></a>
+	                    <ul class="vnav-subnav2">
+	                        <li><a href="">VOLVER A MENU PRINCIPAL</a></li>
+	                    </ul>
+	                </li>
+	                <li class="active2">
+	                    <a href="" class="vnav-item2">PERSONAL<span class="vnav-counter2">2</span></a>
+	                    <ul class="vnav-subnav2">
+	                        <li><a href="index.html">REGISTRAR NUEVO PERSONAL</a></li>
+	                        <li><a href="index.html">LISTAR PERSONAL EN GENERAL</a></li>
+	                    </ul>
+	                </li>
+	                <li class="active2">
+	                    <a href="" class="vnav-item2">CONSULTAS Y REPORTES<span class="vnav-counter2">5</span></a>
+	                    <ul class="vnav-subnav2">
+	                        <li><a href="index.html">FECHA DE REGISTRO</a></li>
+	                        <li><a href="index.html">NOMBRE COMPLETO O CI</a></li>
+	                        <li><a href="index.html">POR PROFESION</a></li>
+	                        <li><a href="index.html">GENERO (M/F)</a></li>
+	                        <li><a href="index.html">POR ESTADO</a></li>
+	                    </ul>
+	                </li>
+	                <li class="active2">
+	                    <a href="" class="vnav-item2">CUENTA DE USUARIO<span class="vnav-counter2">2</span></a>
+	                    <ul class="vnav-subnav2">
+	                        <li><a href="index.html">CAMBIAR CONTRASEÑA</a></li>
+	                        <li><a href="index.html">CERRAR SESION</a></li>
+	                    </ul>
+	                </li>
+	            </ul>
+	        </nav> 
+		</div><!-- cA -->
+		<div id="cB">
+			contenido contenido contenido contenido contenido contenido contenido contenido contenido contenido contenido contenido	contenido contenido contenido contenido contenido contenido
+		</div><!-- cB -->
+		<div class="Cpad">
+			<br class="clear" /></div>
+		</div><!-- Cpad -->
+	</div><!-- content -->
+	<div id="properspace"></div><!-- properspace -->
+</div><!-- daddy -->
+<div id="footer">
+	<div id="foot">
+		<div id="foot1"><a href="">&copy; Copyright 2015</a> - Sistemas UET</div><!-- foot1 -->
+		<div id="foot2">
+			<span class="valid"><a href="">LA PAZ</a> - <a href="">BOLIVIA</a></span><center>Av. Mariscal Santa Cruz Edif. Mutual la Primera <a href=""> Bloque B Piso 19 y 20<span class="star">*</span></a><br>Telf. 2 - <a href="">2141534</a> Fax. 2-<a href="">2141565</a></center>
+		</div><!-- foot1 -->
+	</div><!-- foot -->
+</div><!-- footer -->
+</body>
+</html>
